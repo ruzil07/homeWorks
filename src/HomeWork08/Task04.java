fc32fa87b085f3b23f18bd351138ba6e0c92e205
Рузиль <ruzil.gil1@gmail.com>
2025-04-16 03:02:48 +0500

    Commit HomeWork08 С исправленными домашними заданиями

--- src/HomeWork08/Task04.java
+++ src/HomeWork08/Task04.java
@@ -2,6 +2,24 @@ package HomeWork08;
 
 public class Task04 {
     public static void main(String[] args) {
+        System.out.println("Задание 4");
+        int[] payments = new int[5];
+        payments[0] = 3;
+        payments[1] = 5;
+        payments[2] = 6;
+        payments[3] = 8;
+        payments[4] = 1;
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        System.out.println(reverseFullName);
+
+        for (int i = 0; i < reverseFullName.length / 2; i++) {
+            int index = reverseFullName.length - 1 - i;
+
+            char value = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[index];
+            reverseFullName[index] = value;
+        }
+        System.out.println(reverseFullName);
 
     }
 }
