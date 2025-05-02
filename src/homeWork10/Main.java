c53dcc3d307f7ecfbef086082fa6e0bc62256a91
Рузиль <ruzil.gil1@gmail.com>
2025-05-02 22:00:19 +0500

    Commit HomeWork10 исправил

--- src/homeWork10/Main.java
+++ src/homeWork10/Main.java
@@ -6,34 +6,32 @@ public class Main {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("Задание 1");
-        String res = checkLeapYear(2100);
-        System.out.println(res);
+        checkLeapYear(2100);
 
         System.out.println();
         System.out.println("Задание 2");
 
         String osName = "iOS";
-        getClientOS(0, 2010);
+        printInfoAboutVersion(0, 2010);
 
         System.out.println();
         System.out.println("Задание 3");
 
         int deliveryDistance1 = 110;
-        String result3 = calculateDeliveryDays(deliveryDistance1);
-        System.out.println(result3);
+        System.out.println(calculateDeliveryDays(deliveryDistance1));
     }
 
     // Метод задания 1
-    public static String checkLeapYear(int year) {
+    public static void checkLeapYear(int year) {
         if ((year % 4 == 0) && year % 100 != 0 || year % 400 == 0) {
-            return year + " год високосный ";
+            System.out.println(year + " год високосный ");
         } else {
-            return year + " год не високосный ";
+            System.out.println(year + " год не високосный ");
         }
 // Метот задания 2
     }
 
-    public static void getClientOS(int type, int year) {
+    public static void printInfoAboutVersion(int type, int year) {
         String name;
         if (type == 0) {
             name = "IOS";
@@ -50,16 +48,16 @@ public class Main {
     }
 
     // Метот задания 3
-    public static String calculateDeliveryDays(int deliveryDistance) {
+    public static int calculateDeliveryDays(int deliveryDistance) {
         int count = 1;
         if (deliveryDistance > 20 && deliveryDistance <= 60) {
             count = 2;
         } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
             count = 3;
         } else if (deliveryDistance > 100) {
-            return "Доставки нет";
+            return count;
         }
-        return "Потребуется дней: " + count;
+        return count;
     }
 
 }
